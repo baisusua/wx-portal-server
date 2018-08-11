88a3c39c63e400867fa539007099786ad8fb2c3c
kround <yuan40kang@126.com>
2018-08-11 13:37:52 +0800

    格式化

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,13 +3,13 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-	async index() {
-		const list = await this.ctx.service.list.getList();
-		this.ctx.body = {
-			data:list,
-			total:list.length
-		};
-	}
+    async index() {
+        const list = await this.ctx.service.list.getList();
+        this.ctx.body = {
+            data: list,
+            total: list.length
+        };
+    }
 }
 
 module.exports = HomeController;
--- app/router.js
+++ app/router.js
@@ -4,10 +4,10 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.get('/list', controller.home.index);
-  router.get('/export', controller.excel.index);
-  router.post('/user/create',controller.user.create);
-  app.all('/user/token', app.oAuth2Server.token());
-  app.get('/user/authenticate', app.oAuth2Server.authenticate(), 'user.authenticate');
+    const { router, controller } = app;
+    router.get('/list', controller.home.index);
+    router.get('/export', controller.excel.index);
+    router.post('/user/create', controller.user.create);
+    app.all('/user/token', app.oAuth2Server.token());
+    app.get('/user/authenticate', app.oAuth2Server.authenticate(), 'user.authenticate');
 };
